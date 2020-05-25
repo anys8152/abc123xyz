a4b2586b7de271ba4f140d1af7a999566c8df031
anys8152 <anys8152@naver.om>
2020-05-25 16:06:35 +0900

    스프링5 라이브러리와 VO파일 추가

---
+++ src/main/java/team/vo/ProductVO.java
@@ -0,0 +1,12 @@
+package team.vo;
+
+/**
+ * Created by User on 2020-05-25.
+ */
+public class ProductVO {
+    private String name;
+
+    public ProductVO(String name) {
+        this.name = name;
+    }
+}
